b4caa2648df651f6e301c817f22cef79a7b14102
FRNBDN <francesco.badini94@gmail.com>
2023-02-10 16:05:27 +0000

    update user model last_seen field

--- account/models.py
+++ account/models.py
@@ -42,7 +42,7 @@ class Account(AbstractBaseUser):
     username = models.CharField(max_length=30, unique=True)
     date_joined = models.TimeField(auto_now_add=True,
                                    verbose_name='date joined')
-    last_login = models.DateField(auto_now=True, verbose_name='last seen')
+    last_login = models.DateTimeField(auto_now=True, verbose_name='last seen')
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
