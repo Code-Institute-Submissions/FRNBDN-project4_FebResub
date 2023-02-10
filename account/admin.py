103e1632fb1455b6fea683835b44824584dd5c9e
FRNBDN <francesco.badini94@gmail.com>
2023-02-10 17:04:24 +0000

    update date_joined in account models

--- account/admin.py
+++ account/admin.py
@@ -5,11 +5,11 @@ from account.models import Account
 
 @admin.register(Account)
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'username', 'date_joined', 'last_login',
-                    'is_admin', 'is_staff', 'is_active')
+    list_display = ('email', 'username', 'last_login',
+                    'is_admin', 'is_staff', 'is_active', 'date_joined',)
     search_fields = ('email', 'username')
     readonly_fields = ('date_joined', 'last_login',)
-    list_filter = ('date_joined', 'last_login', 'is_admin', 'is_staff',
-                                                            'is_active')
+    list_filter = ('last_login', 'is_admin',
+                   'is_staff', 'date_joined', 'is_active')
     filter_horizontal = ()
     fieldsets = ()
